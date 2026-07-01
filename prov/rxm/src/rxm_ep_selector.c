39ddf765a8c2e8934376ff5d6ac50309000a7396
Gad Arbel <gad.arbel@intel.com>
2026-07-01 05:22:51 -0700

    sar trough ep0

--- prov/rxm/src/rxm_ep_selector.c
+++ prov/rxm/src/rxm_ep_selector.c
@@ -64,39 +64,7 @@ static uint8_t rxm_rr_select(struct rxm_conn *conn, const struct rxm_pkt *pkt)
                 // RMA / rndv-RMA .
 		return rxm_rr_next(rr, conn);
 
-	if (OFI_LIKELY(pkt->ctrl_hdr.type != rxm_ctrl_seg))
-                // Not SAR
-		return 0;
-
-	seg_type = rxm_sar_get_seg_type((struct ofi_ctrl_hdr *) &pkt->ctrl_hdr);
-	msg_id = pkt->ctrl_hdr.msg_id;
-
-	switch (seg_type) {
-	case RXM_SAR_SEG_MIDDLE:
-		slot = ofi_idm_lookup(&rr->sar_pins, (int) msg_id);
-		if (slot)
-			return (uint8_t)((uintptr_t) slot - 1);
-		idx = rxm_rr_next(rr, conn);
-		/* On map-grow OOM, fall back to ep 0 for the rest of this
-		 * SAR message. Subsequent segments will also miss the
-		 * lookup and land on 0, preserving in-order delivery. */
-		if (OFI_UNLIKELY(ofi_idm_set(&rr->sar_pins, (int) msg_id,
-				(void *)(uintptr_t)(idx + 1)) < 0))
-			return 0;
-		return idx;
-
-	case RXM_SAR_SEG_LAST:
-		slot = ofi_idm_lookup(&rr->sar_pins, (int) msg_id);
-		if (slot) {
-			ofi_idm_clear(&rr->sar_pins, (int) msg_id);
-			return (uint8_t)((uintptr_t) slot - 1);
-		}
-		return rxm_rr_next(rr, conn);
-
-	default:
-                // SAR first
-		return 0;
-	}
+	return 0;
 }
 
 static void rxm_rr_destroy(struct rxm_ep_selector *sel)
